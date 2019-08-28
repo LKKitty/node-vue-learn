a55484df20af0b13190b0d0cb12c1dbb43bcde24
LKKitty <LKKitty@git.com>
2019-08-28 12:29:16 +0800

    fix upload Url

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -50,7 +50,7 @@ module.exports =app => {
   const upload = multer({dest: __dirname + '../../../uploads'})
   app.post('/admin/api/upload',authMiddleware(),upload.single('file'), async (req,res) => {
     const file = req.file
-    file.url = `http://localhost:3000/uploads/${file.filename}`
+    file.url = `http://47.91.212.162:3000/uploads/${file.filename}`
     res.send(file)
     })
   
